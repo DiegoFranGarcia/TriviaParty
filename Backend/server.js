7bfad675e62a6ca4fe73453aa0d5073858c6dc84
Diego Garcia <diegogarcia@atm0561892dyn.dhcp.missouri.edu>
2025-05-07 10:33:45 -0500

    database now connects

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-
+require('dotenv').config({
+    path: require('path').resolve(__dirname, '../.env')
+  });
 const express = require('express'); 
 const app = express();
-const port = 3000;
+const port = 5000;
 const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB')); 
 
 app.use(express.json());
 
-const Router = require('./routes/routes.js'); 
-app.use('/routes', Router);
+const userRouter = require('./routes/users.js'); 
+app.use('/users', userRouter);
 
 app.listen(port, () => console.log(`Server is running`));
